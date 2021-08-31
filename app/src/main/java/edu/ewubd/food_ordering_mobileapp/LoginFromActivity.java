8707554d736b444fcf9231b02c89ba78522fb427
srsizan <samiunsizan@gmail.com>
2021-08-31 15:55:45 +0600

    xml file for login and signup added

--- app/src/main/java/edu/ewubd/food_ordering_mobileapp/LoginFromActivity.java
+++ app/src/main/java/edu/ewubd/food_ordering_mobileapp/LoginFromActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SignupFormActivity extends AppCompatActivity {
+public class LoginFromActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup_form);
+        setContentView(R.layout.activity_login_from);
     }
 }
