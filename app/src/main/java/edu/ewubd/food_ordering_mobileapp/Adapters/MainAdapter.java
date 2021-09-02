78418da3aa8ce0eee601dae4ad7b2b51769ce3e9
KashSaif <2018-2-60-001@std.ewubd.edu>
2021-09-03 02:14:43 +0600

    commited

---
+++ app/src/main/java/edu/ewubd/food_ordering_mobileapp/Adapters/MainAdapter.java
@@ -0,0 +1,68 @@
+package edu.ewubd.food_ordering_mobileapp.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+
+import java.util.ArrayList;
+
+import edu.ewubd.food_ordering_mobileapp.Models.MainModel;
+import edu.ewubd.food_ordering_mobileapp.R;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.viewholder>{
+
+
+    ArrayList<MainModel> list ;
+    Context context;
+
+    public MainAdapter(ArrayList<MainModel> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.sample_mainfood,parent,false);
+        return new viewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewholder holder, int position) {
+
+        final MainModel model = list.get(position);
+        holder.foodimage.setImageResource(model.getImage());
+        holder.mainName.setText(model.getName());
+        holder.price.setText(model.getPrice());
+        holder.description.setText(model.getDescription());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class viewholder extends ViewHolder{
+
+        ImageView foodimage;
+        TextView mainName, price , description;
+
+        public viewholder(@NonNull View itemView){
+            super(itemView);
+
+            foodimage = itemView.findViewById(R.id.imageView);
+            mainName = itemView.findViewById(R.id.name);
+            price = itemView.findViewById(R.id.orderPrice);
+            description = itemView.findViewById(R.id.description);
+
+        }
+    }
+}
--- app/src/main/java/edu/ewubd/food_ordering_mobileapp/HomeActivity.java
+++ app/src/main/java/edu/ewubd/food_ordering_mobileapp/HomeActivity.java
@@ -1,13 +1,42 @@
 package edu.ewubd.food_ordering_mobileapp;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import java.util.ArrayList;
+
+import edu.ewubd.food_ordering_mobileapp.Adapters.MainAdapter;
+import edu.ewubd.food_ordering_mobileapp.Models.MainModel;
+import edu.ewubd.food_ordering_mobileapp.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ArrayList<MainModel> list = new ArrayList<>();
+
+        list.add(new MainModel(R.drawable.chicken_burger, "Burger","5","Chicken Burger with Extra Cheese"));
+        list.add(new MainModel(R.drawable.pepp_pizza, "Pizza","10","Beef Pepperoni Pizza"));
+        list.add(new MainModel(R.drawable.chick_pizza, "Chicken Pizza","0","Chicken Pizza with Extra Cheese"));
+        list.add(new MainModel(R.drawable.cheese_cake, "Cheese Cake","12","Cheese Cake with Blueberry Sauce"));
+        list.add(new MainModel(R.drawable.chicken_wings, "Chicken Wings","5","Chicken Wings with Spices"));
+        list.add(new MainModel(R.drawable.meat_balls, "Meat Balls","5","Spicy Meat Balls"));
+        list.add(new MainModel(R.drawable.choco_cake, "Chocolate Cake","8","Chocolate Cake with Chocolate Frosting"));
+        list.add(new MainModel(R.drawable.soft_drinks, "Soft Drinks","5","Pepsi/Sprite"));
+        list.add(new MainModel(R.drawable.french_fries, "French Fry","10","French Fry with Spices"));
+        list.add(new MainModel(R.drawable.kacchi, "Rice","20","Rice with mutton"));
+
+        MainAdapter adapter = new MainAdapter(list , this);
+        binding.recyclerview.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        binding.recyclerview.setLayoutManager(layoutManager);
     }
+
 }
